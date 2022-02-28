61e8bfffc8195a8a4f88354bc3fe87ce899a62a1
Tavita Su'a <tavitas@sprep.org>
2022-02-28 16:30:22 +1300

    Initial commit

---
+++ scripts/populateTypesenseIndex.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const Typesense = require("typesense");
+
+module.exports = (async () => {
+  const TYPESENSE_CONFIG = {
+    nodes: [
+      {
+        host: process.env.TYPESENSE_HOST,
+        port: process.env.TYPESENSE_PORT,
+        protocol: process.env.TYPESENSE_PROTOCOL,
+      },
+    ],
+    apiKey: process.env.TYPESENSE_ADMIN_API_KEY,
+  };
+
+  const typesense = new Typesense.Client(TYPESENSE_CONFIG);
+})();
