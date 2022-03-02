ccb531c59b1de3f5ea844e3d9e602f6ad54e5061
Tavita Su'a <tavitas@sprep.org>
2022-03-02 14:10:16 +1300

    added basic frontend search widget from instantsearch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { InstantSearch, SearchBox } from "react-instantsearch-dom";
+import { searchClient } from "./typesenseAdapter";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em 0;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <AppContainer>
+      <InstantSearch searchClient={searchClient}>
+        <h2>
+          React Typesense Pacific Environment Portal Dataset InstantSearch
+        </h2>
+        <h4>Search Datasets</h4>
+        <SearchBox />
+      </InstantSearch>
+    </AppContainer>
   );
 }
 
